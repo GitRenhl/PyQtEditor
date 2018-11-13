8fd76cc3854ac44892dac63fc656132606b26988
GitRenhl <niemam10@gmail.com>
2018-11-13 09:11:47 +0100

    Update replace and find

--- src/find_and_replace.py
+++ src/find_and_replace.py
@@ -3,7 +3,8 @@ from PyQt5.QtWidgets import QWidget, \
     QVBoxLayout,\
     QDialog,\
     QLineEdit,\
-    QLabel,\
+    QLabel, \
+    QCheckBox, \
     QPushButton
 from PyQt5.QtCore import Qt
 
@@ -16,8 +17,11 @@ class FindAndReplace(QDialog):
         self.find = QLineEdit()
         self.replace = QLineEdit()
 
+        self.whole_words = QCheckBox()
+        self.case_sensitive = QCheckBox()
+        self.wraps = QCheckBox()
+
         self.status_bar = QLabel()
-        self.status_bar.setText("Simple Text")
 
         self.setup_ui()
 
@@ -25,64 +29,127 @@ class FindAndReplace(QDialog):
 
     def setup_ui(self):
         self.setWindowTitle("Replace text")
-        self.resize(300, 80)
-        self.setWindowModality(Qt.ApplicationModal)
+        self.setFixedSize(350, 180)
+        # self.setWindowModality(Qt.ApplicationModal)
+        self.setModal(False)
+        self.find.setFixedWidth(160)
+        self.replace.setFixedWidth(160)
 
         main_lay = QVBoxLayout()
-        lay1 = QVBoxLayout()
-        lay1_1 = QHBoxLayout()
-        lay1_2 = QHBoxLayout()
+        lay_inputs_and_buttons = QHBoxLayout()
+        lay_inputs = QVBoxLayout()
 
-        lay2 = QVBoxLayout()
+        lay_input_text = QVBoxLayout()
+        lay_input_text_1 = QHBoxLayout()
+        lay_input_text_2 = QHBoxLayout()
 
-        # LAY 1_1
+        lay_checkboxes = QVBoxLayout()
+
+        lay_buttons = QVBoxLayout()
+
+        ley_msg = QVBoxLayout()
+
+        # TEXT #
         label_find = QLabel()
         label_find.setText("Find:")
-        lay1_1.addWidget(label_find)
-        lay1_1.addWidget(self.find)
+        label_find.setAlignment(Qt.AlignRight)
+        lay_input_text_1.addWidget(label_find)
+        lay_input_text_1.addWidget(self.find)
 
-        # LAY 1_2
         label_replace = QLabel()
         label_replace.setText("Replace:")
-        lay1_2.addWidget(label_replace)
-        lay1_2.addWidget(self.replace)
+        label_replace.setAlignment(Qt.AlignRight)
+        lay_input_text_2.addWidget(label_replace)
+        lay_input_text_2.addWidget(self.replace)
 
-        # LAY 1 #
-        lay1.addLayout(lay1_1)
-        lay1.addLayout(lay1_2)
+        lay_input_text.addLayout(lay_input_text_1)
+        lay_input_text.addLayout(lay_input_text_2)
 
-        # LAY 2 #
-        btn_replace_all = QPushButton("Replace all")
-        btn_replace_all.clicked.connect(self.__btn_replace_all_click)
-        btn_count = QPushButton("Count")
-        btn_count.clicked.connect(self.__btn_count)
-        btn_exit = QPushButton("Exit")
-        btn_exit.clicked.connect(self.close)
-        lay2.addWidget(btn_replace_all)
-        lay2.addWidget(btn_count)
-        lay2.addWidget(btn_exit)
+        # CHECKBOXES #
+        # TODO read from cache
+        self.whole_words.setChecked(False)
+        self.whole_words.setText("Match case")
+        self.case_sensitive.setChecked(False)
+        self.case_sensitive.setText("Match whole words only")
+        self.wraps.setChecked(True)
+        self.wraps.setText("Wraps around")
+
+        lay_checkboxes.addWidget(self.whole_words)
+        lay_checkboxes.addWidget(self.case_sensitive)
+        lay_checkboxes.addWidget(self.wraps)
+
+        # LAY INPUTS #
+        lay_inputs.addLayout(lay_input_text)
+        lay_inputs.addLayout(lay_checkboxes)
+
+        # LAY BUTTONS #
+        buttons = {
+            'find': QPushButton("Find Next"),
+            'replace': QPushButton("Replace"),
+            'replace_all': QPushButton("Replace all"),
+            'count': QPushButton("Count"),
+            'exit': QPushButton("Exit"),
+        }
+
+        buttons['find'].clicked.connect(self.__btn_find)
+        buttons['replace'].clicked.connect(self.__btn_replace_click)
+        buttons['replace_all'].clicked.connect(self.__btn_replace_all_click)
+        buttons['count'].clicked.connect(self.__btn_count)
+        buttons['exit'].clicked.connect(self.close)
+
+        for b in buttons.values():
+            lay_buttons.addWidget(b)
+
+        # LAY INPUTS AND BUTTONS #
+        lay_inputs_and_buttons.addLayout(lay_inputs)
+        lay_inputs_and_buttons.addLayout(lay_buttons)
 
         # MESSAGE LEY
-        ley_msg = QVBoxLayout()
         ley_msg.addWidget(self.status_bar)
 
-        # ACTION LAY
-        action_lay = QHBoxLayout()
-        action_lay.addLayout(lay1)
-        action_lay.addLayout(lay2)
-
         # MAIN LAY #
-        main_lay.addLayout(action_lay)
+        main_lay.addLayout(lay_inputs_and_buttons)
         main_lay.addLayout(ley_msg)
 
         self.setLayout(main_lay)
 
+    def __btn_replace_click(self):
+        self.master.files_tabs.currentWidget().replace(self.replace.text())
+
     def __btn_replace_all_click(self):
-        old, new = self.find.text(), self.replace.text()
-        print("replace", old, new)
-        self.master.files_tabs.replace_in_text(
-            old, new
-        )
+        self.status_bar.setText('')
+        curWidg = self.master.files_tabs.currentWidget
+        replaced = 0
+        founded = True
+        while True:
+            founded = curWidg().findFirst(self.find.text(),
+                                          True,
+                                          self.case_sensitive.isChecked(),
+                                          self.whole_words.isChecked(),
+                                          self.wraps.isChecked())
+            if founded:
+                curWidg().replace(self.replace.text())
+                replaced += 1
+            else:
+                self.status_bar.setStyleSheet("QLabel {color: red;}")
+                self.status_bar.setText(
+                    f"{replaced} occurrences were replaced")
+                break
+
+    def __btn_find(self):
+        curWidg = self.master.files_tabs.currentWidget
+        founded = curWidg().findFirst(self.find.text(),
+                                      True,
+                                      self.case_sensitive.isChecked(),
+                                      self.whole_words.isChecked(),
+                                      self.wraps.isChecked())
+        if not founded:
+            self.status_bar.setStyleSheet("QLabel {color: red;}")
+            self.status_bar.setText(
+                "Can't find the text \"{}\"".format(
+                    self.find.text()
+                )
+            )
 
     def __btn_count(self):
         counter = self.master.files_tabs.count_text(self.find.text())
